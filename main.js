18022e5a678e5459ca1dc4e2b9572cc6f8cd2bcb
kanav-bhanushali <71547881+kanav-bhanushali@users.noreply.github.com>
2022-09-26 16:03:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var SpeechRecognition=window.webkitSpeechRecognition;
 var recognition=new SpeechRecognition();
+var content
 
 function start(){
     document.getElementById("textbox").innerHTML="";
@@ -10,3 +11,53 @@ recognition.onresult=function(event){
 content=event.results[0][0].transcript;
 document.getElementById("textbox").innerHTML=content;
 }
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
+function speak(){
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+
+}
+
+function take_snapshot()  {
+  
+  console.log(img_id);
+  Webcam.snap(function(data_uri){
+    if(img_id=='selfie1'){
+      document.getElementById("result1").innerHTML = "<img id='selfie1' src="+data_uri+"/>"
+    }
+    
+     if(img_id=='selfie2'){
+      document.getElementById("result2").innerHTML = "<img id='selfie2' src="+data_uri+"/>"
+    }
+    
+     if(img_id=='selfie3'){
+      document.getElementById("result3").innerHTML = "<img id='selfie3' src="+data_uri+"/>"
+    }
+  });
+}
+
+setTimeout(function()
+          {
+  img_id ="selfie1";
+  take_snapshot()
+  speak_data = "Taking you selfie 10 seconds";
+  var utterThis = new SpeechSynthesisUtterance(speak_data);
+  synth.speak(UtterThis);
+}, 5000);
